4ac942e2cd226ae088a57e59a9af151adcf2c59a
RamonFischer <ramonfischer1999@gmail.com>
2023-04-15 02:06:28 -0300

    changing REST CONTROLLER VALUES

--- src/main/java/com/api/apiDvops/controller/UserController.java
+++ src/main/java/com/api/apiDvops/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "users")
 @CrossOrigin("*")
 public class UserController {
     @Autowired
